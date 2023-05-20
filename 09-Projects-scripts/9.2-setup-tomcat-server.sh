8111584f329de38561b4a08f15846dcdf08ac67a
R L NARAYANAN <97275577+r-narayanan4@users.noreply.github.com>
2023-05-20 12:24:25 +0530

    Updated 9.2-setup-tomcat-server.sh
    improvised the code

--- 09-Projects-scripts/9.2-setup-tomcat-server.sh
+++ 09-Projects-scripts/9.2-setup-tomcat-server.sh
@@ -1,78 +1,108 @@
 #!/usr/bin/env bash
-#Author: RLN
-#Script to install any tomcat version and confgiure the server 
-#(note: if the script failed please check the line number under tomcat-files-configuration with orinalfile in your system)
+# Author: R L Narayanan
+# Script to install and configure Tomcat server
 
-#Update and get wget install in your host
-if ! [[ $(yum update -y) || ! $(yum install wget -y) ]]
-then
-    yum update -y &> /dev/null
-    yum install wget -y &> /dev/null
-fi
-
-#To install Java  
-yum install fontconfig java-11-openjdk -y  &> /dev/null
-
-#Error Handling
-if [[ $# -ne 1 ]]
-then
-    echo "hey admin please run the script as follows"
-    echo "setup-tomcat-server.sh tomcat_version"
+# Update and install wget
+if ! (yum update -y && yum install wget -y); then
+    echo "Failed to update or install wget. Please check your system configuration." >&2
     exit 1
 fi
 
-#Cmd to install tomcat on your host. 
-#Variables
-req_tom_ver=$1
-tom_m_ver=$(echo $req_tom_ver | cut -c 1)
-url="https://dlcdn.apache.org/tomcat/tomcat-${tom_m_ver}/v${req_tom_ver}/bin/apache-tomcat-${req_tom_ver}.tar.gz"
-
-#Create tomcat directory
-cd /opt
-wget $url &>/dev/null
-tar -xvzf apache-tomcat-${req_tom_ver}.tar.gz &>/dev/null
-mv apache-tomcat-${req_tom_ver} tomcat${tom_m_ver} 
-rm -rf apache-tomcat-${req_tom_ver}.tar.gz
-if [[ $? -eq 0 ]]
-then 
-    echo "tomcat is installed successfully"
+# Install Java
+if ! yum install fontconfig java-11-openjdk -y &> /dev/null; then
+    echo "Failed to install Java." >&2
+    exit 1
 fi
 
-#Create link files for tomcat startup.sh and shutdown.sh
-ln -s /opt/tomcat${tom_m_ver}/bin/startup.sh /usr/local/bin/tomcatup
-ln -s /opt/tomcat${tom_m_ver}/bin/shutdown.sh /usr/local/bin/tomcatdown
+# Check if Tomcat is already installed
+if [[ -d "/opt/tomcat${tom_m_ver}" ]]; then
+    read -p "Tomcat is already installed. Do you want to reinstall? [y/n]: " reinstall
+    if [[ $reinstall != "y" ]]; then
+        echo "Script terminated by user."
+        exit 0
+    fi
+    rm -rf "/opt/tomcat${tom_m_ver}"
+fi
 
-#-----------------------------------------TomCat-Files-Configuration----------------------------------------------------
+# Get user input for Tomcat version and port number
+read -p "Enter Tomcat version to install (e.g., 9.0.48): " req_tom_ver
+read -p "Enter the desired port number (default: 8089): " port_number
 
-#To change the port number  8080 to 8089 (access tomcat application from browser on prot 8090)
-sed -i '69s/8080/8089/' /opt/tomcat${tom_m_ver}/conf/server.xml
+# Set default port if not provided by the user
+port_number=${port_number:-8089}
 
-#To modify this restriction you'll need to edit the Manager's context.xml file. (under webapps dir)
-#We need to  find context.xml and  comment () Valve ClassName under host-manager and manager.
+# Download and install Tomcat
+tom_m_ver=$(echo "$req_tom_ver" | cut -c 1)
+url="https://dlcdn.apache.org/tomcat/tomcat-${tom_m_ver}/v${req_tom_ver}/bin/apache-tomcat-${req_tom_ver}.tar.gz"
 
-sed -i '21s/^/<!-- /' /opt/tomcat${tom_m_ver}/webapps/host-manager/META-INF/context.xml
-sed -i '22s/$/ -->/' /opt/tomcat${tom_m_ver}/webapps/host-manager/META-INF/context.xml
-sed -i '21s/^/<!-- /' /opt/tomcat${tom_m_ver}/webapps/manager/META-INF/context.xml
-sed -i '22s/$/ -->/' /opt/tomcat${tom_m_ver}/webapps/manager/META-INF/context.xml
+cd /opt
+if ! wget "$url" &>/dev/null; then
+    echo "Failed to download Tomcat archive. Please check the Tomcat version and your network connection." >&2
+    exit 1
+fi
 
-#We need to update the user information in users.xml in /conf
-line_number=48  # Replace with the line number where you want to add the content
-file_path=/opt/tomcat${tom_m_ver}/conf/tomcat-users.xml  # Replace with the path to your file
+if ! tar -xvzf "apache-tomcat-${req_tom_ver}.tar.gz" &>/dev/null; then
+    echo "Failed to extract Tomcat archive." >&2
+    exit 1
+fi
 
-#Use sed to insert the content after the specified line number
-sed -i "${line_number}a\\
-<role rolename=\"manager-gui\"/>\\
-<role rolename=\"manager-script\"/>\\
-<role rolename=\"manager-jmx\"/>\\
-<role rolename=\"manager-status\"/>\\
-<user username=\"admin\" password=\"admin\" roles=\"manager-gui, manager-script, manager-jmx, manager-status\"/>\\
-<user username=\"deployer\" password=\"deployer\" roles=\"manager-script\"/>\\
-<user username=\"tomcat\" password=\"s3cret\" roles=\"manager-gui\"/>" "${file_path}"
+mv "apache-tomcat-${req_tom_ver}" "tomcat${tom_m_ver}"
+rm -rf "apache-tomcat-${req_tom_ver}.tar.gz"
 
-#Restart the tomcat
+# Create symbolic links for startup and shutdown scripts
+if ! ln -s "/opt/tomcat${tom_m_ver}/bin/startup.sh" "/usr/local/bin/tomcatup"; then
+    echo "Failed to create symbolic link for startup.sh." >&2
+    exit 1
+fi
+
+if ! ln -s "/opt/tomcat${tom_m_ver}/bin/shutdown.sh" "/usr/local/bin/tomcatdown"; then
+    echo "Failed to create symbolic link for shutdown.sh." >&2
+    exit 1
+fi
+
+# Update Tomcat configuration files
+if ! sed -i "s/8080/$port_number/" "/opt/tomcat${tom_m_ver}/conf/server.xml"; then
+    echo "Failed to update server.xml." >&2
+    exit 1
+fi
+
+if ! sed -i '21s/^/<!-- /' "/opt/tomcat${tom_m_ver}/webapps/host-manager/META-INF/context.xml"; then
+    echo "Failed to update host-manager context.xml." >&2
+    exit 1
+fi
+
+if ! sed -i '22s/$/ -->/' "/opt/tomcat${tom_m_ver}/webapps/host-manager/META-INF/context.xml"; then
+    echo "Failed to update host-manager context.xml." >&2
+    exit 1
+fi
+
+if ! sed -i '21s/^/<!-- /' "/opt/tomcat${tom_m_ver}/webapps/manager/META-INF/context.xml"; then
+    echo "Failed to update manager context.xml." >&2
+    exit 1
+fi
+
+if ! sed -i '22s/$/ -->/' "/opt/tomcat${tom_m_ver}/webapps/manager/META-INF/context.xml"; then
+    echo "Failed to update manager context.xml." >&2
+    exit 1
+fi
+
+line_number=48
+file_path="/opt/tomcat${tom_m_ver}/conf/tomcat-users.xml"
+user_info="<role rolename=\"manager-gui\"/>\n<role rolename=\"manager-script\"/>\n<role rolename=\"manager-jmx\"/>\n<role rolename=\"manager-status\"/>\n<user username=\"admin\" password=\"admin\" roles=\"manager-gui, manager-script, manager-jmx, manager-status\"/>\n<user username=\"deployer\" password=\"deployer\" roles=\"manager-script\"/>\n<user username=\"tomcat\" password=\"s3cret\" roles=\"manager-gui\"/>"
+
+if ! sed -i "${line_number}a\\$user_info" "$file_path"; then
+    echo "Failed to update tomcat-users.xml." >&2
+    exit 1
+fi
+
+# Restart Tomcat
 tomcatdown &>/dev/null
 tomcatup &>/dev/null
 
-#Message after completing configuration
-echo "Tomcat server configuration completed..."
-echo "Access Tomcat server using password with hostIp:port no"  # if this shows error in browser check the firewall and allow port no.
+# Get host IP
+host_ip=$(hostname -I | awk '{print $1}')
+
+# Print completion message
+echo "Tomcat server configuration completed."
+echo "Access Tomcat server using the following URL:"
+echo "http://${host_ip}:${port_number}"
